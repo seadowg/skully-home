cc4b960f377d0f0ca537c6b7169e22f75ac3b12a
Callum Stott <cstott@pivotal.io>
2017-08-18 17:05:21 +0100

    Handle failure cases

--- src/main/kotlin/com/seadowg/skullyhome/handler/EpisodeLookup.kt
+++ src/main/kotlin/com/seadowg/skullyhome/handler/EpisodeLookup.kt
@@ -8,16 +8,27 @@ import okhttp3.OkHttpClient
 
 class EpisodeLookup : ActionHandler {
     override fun handle(request: Request): Response {
+        val currentEpisodeName = request.params.getArgument("any")!!
+
         val httpClient = OkHttpClient()
         val skullyRequest = okhttp3.Request.Builder()
                 .get()
-                .url("https://skully.herokuapp.com/episodes/search?name=${request.params.getArgument("any")}")
+                .url("https://skully.herokuapp.com/episodes/search?name=$currentEpisodeName")
                 .build()
 
-        val skullResponse = httpClient.newCall(skullyRequest).execute()
+        val skullyResponse = httpClient.newCall(skullyRequest).execute()
 
-        val episode = Gson().fromJson(skullResponse.body()!!.string(), Episode::class.java)
-        return Response("Skip to ${episode.next}", emptyList())
+        if (skullyResponse.isSuccessful) {
+            val episode = Gson().fromJson(skullyResponse.body()!!.string(), Episode::class.java)
+
+            return if (currentEpisodeName.toLowerCase() == episode.next.toLowerCase()) {
+                Response("That's a good one! Stick it on.", emptyList())
+            } else {
+                Response("Skip to ${episode.next}", emptyList())
+            }
+        } else {
+            return Response("Hmmmm... I'm not sure that's an episode... in fact I'm not sure I can trust you.", emptyList())
+        }
     }
 }
 
